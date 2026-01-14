17630a0653fa35a978bed09d2e21c4df11c0341b
sohyerim-dev <fnmnn1@gmail.com>
2026-01-14 07:06:54 +0000

    자동빌드 owner 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="border border-amber-200">Yes.us v.01</h1>
+      <h1 className="border border-amber-200">Yes.us v.03</h1>
       <div>프로젝트 화이팅~~~~~~~</div>
     </>
   );
